c49b639810cc62ab0cd19809ae35e2f434c3b958
Max Wittmann <damxam@gmail.com>
2019-02-02 13:12:07 +1100

    ...

--- src/main/scala/templatey/Main.scala
+++ src/main/scala/templatey/Main.scala
@@ -5,10 +5,12 @@ import scala.collection.immutable.TreeSet
 object Main {
 
 
-  sealed trait Primitive
-  case class PInt(i: Int) extends Primitive
-  case class PBoolean(b: Boolean) extends Primitive
-  case class PString(s: String) extends Primitive
+//  sealed trait Primitive
+//  case class PInt(i: Int) extends Primitive
+//  case class PBoolean(b: Boolean) extends Primitive
+//  case class PString(s: String) extends Primitive
+
+  case class Primitive(key: String)
 
   sealed trait Ele
 
@@ -30,6 +32,10 @@ object Main {
 
     implicit def toWith(vertex: Vertex): WithVertex = WithVertex(vertex, None)
 
+    implicit def toWith(vertices: List[Vertex]): List[WithVertex] = vertices.map(vertex => WithVertex(vertex, None))
+
+    implicit def toPath(vertex: Vertex): Path = Path(vertex, List.empty)
+
   }
 
   import Implicits._
@@ -37,45 +43,53 @@ object Main {
 
 
   /*
+
+
+      MATCH
+        (workflowDefinition :WORKFLOW_DEFINITION { uid: wdUid })
+      WITH workflowDefinition
       CREATE
-        (n ...) - ....,
-      WITH (n), ...
-
-
-
-
+        (workflowInstance :WORKFLOW_INSTANCE { uid: data.uid } ) -[DEFINED_BY]-> (workflowDefinition)
+      WITH workflowDefinition, workflowInstance
+      UNWIND data.inputs AS inputArtifact
+        MATCH
+          (inArti :ARTIFACT { uid: inputArtifact.uid })
+        WITH workflowDefinition, workflowInstance, inArti
+        CREATE
+          (workflowInstance) -[:HAS_INPUT]-> (inArti)
+      WITH workflowDefinition, workflowInstance, COLLECT(inArti) AS inputArtifacts
+      UNWIND data.outputs AS outputArtifact
+        CREATE
+          (workflowInstance) -[:PRODUCES_OUTPUT]-> (outArti :ARTIFACT { uid: outputArtifact.uid })
+      WITH workflowDefinition, workflowInstance, inputArtifacts, COLLECT(outArti) AS outputArtifacts
+      RETURN workflowDefinition, workflowInstance, inputArtifacts, outputArtifacts
    */
 
 
+  def test2(): Unit = {
+    val workflowDefinitionFull =
+      FullVertex("workflowDefinition", TreeSet("WORKFLOW_DEFINITION"), Map("uid" -> Primitive("$definitionUid")))
+
+    val workflowInstanceFull =
+      FullVertex("workflowInstance", TreeSet("WORKFLOW_INSTANCE"), Map("uid" -> Primitive("$instanceUid")))
+
+    val workflowDefinitionRef = fullToReferenceVertex(workflowDefinitionFull)
+    val workflowInstanceRef = fullToReferenceVertex(workflowInstanceFull)
 
 
-  def main(args: Array[String]): Unit = {
+    val inArtiFull =
+      FullVertex("inArt", TreeSet("ARTIFACT"), Map("uid" -> Primitive("inputArtifact.uid")))
 
-    /*
-
-    for {
-
-      aAttrs <-
-      a <- defineVertex(aAttrs)
-      b <- defineVertex(...)
-      c <- defineVertex(...)
-
-      _ <- create(a, List(("EDGE_TO", b), ("EDGE_TO", c))
-
+    renderMatch(workflowDefinitionFull)
+    renderWith(workflowDefinitionFull)
+    renderCreate(Path(workflowInstanceFull, List(("DEFINED_BY", workflowDefinitionRef))))
+    renderWith(List(workflowDefinitionFull, workflowInstanceFull))
+    renderUnwind(Primitive("$inputs"), "inputArtifact")
+    renderMatch(inArtiFull)
+    renderWith(List(workflowDefinitionFull, workflowInstanceFull, inArtiFull))
+    renderCreate(Path(workflowInstanceRef, List(("HAS_INPUT", workflowDefinitionRef))))
 
 
-
-    } yield {
-
-    }
-
-    */
-
-
-
-    val v1 = VertexRef("a")
-    val v2 = FullVertex("b", TreeSet("B", "B2"), Map("bInt" -> PInt(5)))
-    val v3 = FullVertex("c", TreeSet("C", "C2"), Map("cBool" -> PBoolean(false)))
 
     val p1 = Path(
       v1,
@@ -87,22 +101,49 @@ object Main {
       List(("EDGE_TO_C", v3))
     )
 
+    println(renderCreate(List(p1, p2)))
+    println(renderWith(List(v1, v2, v3)))
+    println(renderReturn(List(v1, v2, v3)))
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def test1(): Unit = {
+    val v1 = VertexRef("a")
+    val v2 = FullVertex("b", TreeSet("B", "B2"), Map("bInt" -> Primitive("data.bInt")))
+    val v3 = FullVertex("c", TreeSet("C", "C2"), Map("cBool" -> Primitive("data.cBool")))
+
+    val p1 = Path(
+      v1,
+      List(("EDGE_TO_B", v2))
+    )
+
+    val p2 = Path(
+      v1,
+      List(("EDGE_TO_C", v3))
+    )
 
     println(renderCreate(List(p1, p2)))
     println(renderWith(List(v1, v2, v3)))
+    println(renderReturn(List(v1, v2, v3)))
   }
 
+
+  def fullToReferenceVertex(full: FullVertex): VertexRef = VertexRef(full.name)
 
   def renderLabels(labels: Set[String]): String = labels.map(l => s":$l").mkString(" ")
 
 
-  def renderAttribute(primitive: Primitive): String = {
-    primitive match {
-      case PInt(i)      => i.toString
-      case PBoolean(b)  => b.toString
-      case PString(s)   => s
-    }
-  }
+  def renderAttribute(primitive: Primitive): String = primitive.key
+//  {
+//    primitive match {
+//      case PInt(i)      => i.toString
+//      case PBoolean(b)  => b.toString
+//      case PString(s)   => s
+//    }
+//  }
 
   def renderAttributes(attributes: Map[String, Primitive]): String = {
     val attrString = attributes.map { case (k, v) => s"$k: ${renderAttribute(v)}" }.mkString(", ")
@@ -120,7 +161,15 @@ object Main {
   def renderCreate(paths: List[Path]): String = {
     val renderedPaths = paths.map(renderPath).mkString(",\n")
     s"""CREATE
-       |$renderedPaths""".stripMargin
+       |${indent(renderedPaths, 3)}""".stripMargin
+  }
+
+  def renderMatch(path: Path): String = renderMatch(List(path))
+
+  def renderMatch(paths: List[Path]): String = {
+    val renderedPaths = paths.map(renderPath).mkString(",\n")
+    s"""MATCH
+       |${indent(renderedPaths, 3)}""".stripMargin
   }
 
   def renderPath(path: Path): String =
@@ -129,9 +178,16 @@ object Main {
     }.reverse.mkString
 
 
+  def renderWith(part: WithPart): String = renderWith(List(part))
+
   def renderWith(parts: List[WithPart]): String = {
     val body = parts.foldLeft(List[String]()) { case (soFar, part) => renderWithPart(part) :: soFar }.reverse.mkString(", ")
     s"WITH $body"
+  }
+
+  def renderReturn(parts: List[WithPart]): String = {
+    val body = parts.foldLeft(List[String]()) { case (soFar, part) => renderWithPart(part) :: soFar }.reverse.mkString(", ")
+    s"RETURN $body"
   }
 
   def renderWithPart(part: WithPart): String = part match {
@@ -147,6 +203,12 @@ object Main {
 
   }
 
+  def renderUnwind(primitive: Primitive, as: String): String =
+    s"UNWIND ($primitive) AS $as"
+
+  def indent(str: String, by: Int = 3): String = {
+    str.split("\n").map(line => s"${" " * by}$line").mkString("\n")
+  }
 
 
 
